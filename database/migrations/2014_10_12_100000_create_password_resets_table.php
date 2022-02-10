8bca554d29e834447d36d0695489c9c1267ef8bf
ssdf <exwx@gmail.com>
2022-02-10 07:03:49 -0800

    Revert "add migration files"
    This reverts commit e6431dab200a61d5e6c81bdc9a927db344e55220.

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAlembicVersionConfigTable extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateAlembicVersionConfigTable extends Migration
      */
     public function up()
     {
-        Schema::create('alembic_version_config', function (Blueprint $table) {
-            $table->string('version_num', 32)->primary();
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -25,6 +27,6 @@ class CreateAlembicVersionConfigTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('alembic_version_config');
+        Schema::dropIfExists('password_resets');
     }
 }
