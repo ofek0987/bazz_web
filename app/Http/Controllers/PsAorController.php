d772c8a510f12f9cac108d6b9d01042b8a91b1c4
ssdf <exwx@gmail.com>
2022-02-10 08:45:49 -0800

    Revert "change controllers to one controller 'UserController'"
    This reverts commit 10988d292dc142adfe4cce0d2765381e4f66a08b.

---
+++ app/Http/Controllers/PsAorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PsAor;
+use Illuminate\Http\Request;
+
+class PsAorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PsAor  $psAor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PsAor $psAor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PsAor  $psAor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PsAor $psAor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PsAor  $psAor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PsAor $psAor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PsAor  $psAor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PsAor $psAor)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PsEndpointController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PsEndpoint;
+use Illuminate\Http\Request;
+
+class PsEndpointController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PsEndpoint  $psEndpoint
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PsEndpoint $psEndpoint)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PsEndpoint  $psEndpoint
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PsEndpoint $psEndpoint)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PsEndpoint  $psEndpoint
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PsEndpoint $psEndpoint)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PsEndpoint  $psEndpoint
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PsEndpoint $psEndpoint)
+    {
+        //
+    }
+}
