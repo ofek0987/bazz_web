c48a99d03a25902dd0fef63d2cd7dbfe4eae1c72
ssdf <exwx@gmail.com>
2022-02-11 05:20:41 -0800

    initial frontend setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
 require('./bootstrap');
+
+
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Home from './components/Home.vue'
+import Login from './components/Login.vue'
+
+const routes = [
+  {path: "/", component: Home},
+  {path: "/login", component: Login}
+]
+
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp({})
+app.use(router)
+app.mount("#app")
